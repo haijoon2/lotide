f70dac8e011831d8b9d767cf7a9ef4c053cce4d9
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-05-21 23:04:36 -0700

    implement without.js

---
+++ without.js
@@ -0,0 +1,42 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const result = eqArrays(actual, expected);
+
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = (array1, array2) => {
+  const result = [];
+  array1.forEach((elem) => {
+    if (!array2.includes(elem)) {
+      result.push(elem);
+    }
+  });
+
+  return result;
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
