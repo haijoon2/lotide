66d0c22aca92b570b4cda5000ac81f12c0e1d086
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-05-21 22:59:25 -0700

    implement assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const result = eqArrays(actual, expected);
+
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
