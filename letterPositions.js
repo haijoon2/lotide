097f5f3ffd527e089fbefb12d7b01417795558d3
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-05-28 19:52:51 -0700

    implement letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const result = eqArrays(actual, expected);
+
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const alphabet = sentence[i];
+
+    if (results[alphabet]) {
+      results[alphabet].push(i);
+    } else {
+      results[alphabet] = [i];
+    }
+  }
+
+  return results;
+};
+
+const result1 = letterPositions("hello");
+const result2 = letterPositions("light house is cool");
