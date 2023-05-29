9db18269a3279c51144730f1489b84c7c3194858
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-05-28 19:51:11 -0700

    implement countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("hello");
+const result2 = countLetters("LHL");
+
+console.log(result1);
+console.log(result2);
+
