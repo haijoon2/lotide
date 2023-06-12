47d49f3f7867e1fe7250d8699c1c6be657ba348e
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-06-11 21:51:39 -0700

    create tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for ['Yo Yo']", () => {
+    assert.deepEqual(tail(['Yo Yo']), []);
+  });
+});
