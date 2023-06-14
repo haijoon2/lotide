c88e2d5ec70c95de4df13ab6292a2763e533575d
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-06-13 22:55:59 -0700

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const countLetters = require('./countLetters');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+  countLetters,
+  head,
+  tail,
+  middle
+};
